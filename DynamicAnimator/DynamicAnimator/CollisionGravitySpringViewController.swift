23a64ed38bf262b44e084fdfb47f7c30526453d0
Victor Chee <victor.qhj@gmail.com>
2015-12-24 13:39:26 +0800

    add a new behaviour

--- DynamicAnimator/DynamicAnimator/CollisionGravitySpringViewController.swift
+++ DynamicAnimator/DynamicAnimator/CollisionGravitySpringViewController.swift
@@ -1,14 +1,14 @@
 //
-//  AttachmentsViewController.swift
+//  CollisionGravitySpringViewController.swift
 //  DynamicAnimator
 //
-//  Created by qihaijun on 12/23/15.
+//  Created by qihaijun on 12/24/15.
 //  Copyright © 2015 VictorChee. All rights reserved.
 //
 
 import UIKit
 
-class AttachmentViewController: UIViewController {
+class CollisionGravitySpringViewController: UIViewController {
     @IBOutlet weak var attachmentView: UIView!
     @IBOutlet weak var itemView: UIView!
     @IBOutlet weak var itemAttachmentView: UIView!
@@ -16,28 +16,30 @@ class AttachmentViewController: UIViewController {
     var animator: UIDynamicAnimator!
     var attachmentBehavior: UIAttachmentBehavior!
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
         animator = UIDynamicAnimator(referenceView: view)
+        
+        let gravityBehavior = UIGravityBehavior(items: [itemView])
         
         let collisionBehavior = UICollisionBehavior(items: [itemView])
         collisionBehavior.translatesReferenceBoundsIntoBoundary = true
         
-        let anchor = CGPoint(x: itemView.center.x, y: itemView.center.y - 100)
-        let offset = UIOffset(horizontal: -22, vertical: -22)
-        attachmentBehavior = UIAttachmentBehavior(item: itemView, offsetFromCenter: offset, attachedToAnchor: anchor)
-//        attachmentBehavior.damping = 0.5
-//        attachmentBehavior.frequency = 0.5
-//        attachmentBehavior.frictionTorque = 0.5
+        let anchorPoint = CGPoint(x: itemView.center.x, y: itemView.center.y - 100)
+        attachmentBehavior = UIAttachmentBehavior(item: itemView, attachedToAnchor: anchorPoint)
+        attachmentBehavior.frequency = 1
+        attachmentBehavior.damping = 0.1
         
         attachmentView.center = attachmentBehavior.anchorPoint
-        itemAttachmentView.center = CGPoint(x: 22, y: 22)
+        itemAttachmentView.center = CGPoint(x: 44, y: 44)
         
-        animator.addBehavior(collisionBehavior)
         animator.addBehavior(attachmentBehavior)
-        
+        animator.addBehavior(collisionBehavior)
+        animator.addBehavior(gravityBehavior)
     }
-    
+
     @IBAction func pan(sender: UIPanGestureRecognizer) {
         attachmentBehavior.anchorPoint = sender.locationInView(view)
         attachmentView.center = attachmentBehavior.anchorPoint
